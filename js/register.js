f31f1e98d673f876838c32abb2e4d2ca24a5f154
Gabriel Morales Martínez <gabrielmorales1996@gmail.com>
2022-05-22 18:07:03 +0200

    front end terminado

--- js/register.js
+++ js/register.js
@@ -1,19 +1,22 @@
 $(document).ready(function(){
-    $("#btn-login").click(function(){
+    $("#btn-register").click(function(){
         let Name = $("#user").val().trim();
+        let Email = $("#email").val().trim();
         let Password = $("#pwd").val().trim();
+        let BirthDate = $("#birthdate").val().trim();
+
+        console.log(BirthDate);
 
         console.log(Name)
 
-        if( Name != "" && Password != "" ){
+        if( Name != "" && Password != "" && Email != ""){
             $.ajax({
-                url:'https://localhost:44367/User/Login',
+                url:'https://localhost:44367/User/Register',
                 type:'post',
-                data:JSON.stringify({name:Name,password:Password}),
+                data:JSON.stringify({name:Name,email:Email,password:Password,birthdate:BirthDate}),
                 contentType:"application/json; charset=utf-8",
                 success:function(response){
-                    
-                    window.location = "welcome.html";
+                    window.location = "index.html";
                 },
                 error:function(){
                     $("#error").fadeIn();
@@ -21,8 +24,4 @@ $(document).ready(function(){
             });
         }
     });
-
-    $("#btn-register").click(function(){
-        window.location = "register.html";
-    })
 });
--- js/welcome.js
+++ js/welcome.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
         
     $.ajax({
-        url:'https://localhost:5001/Alumno/All',
+        url:'https://localhost:44367/Alumno/All',
         type:'get',
         contentType:"application/json; charset=utf-8",
         success:function(response){
             let alumnos = response
             alumnos.forEach(alumno => {
                 console.log(alumno)
-                $("#alumnos").append('<li>'+alumno.dni+'</li>')
+                $("#alumnos").append('<li>'+alumno.nombre +"  "+alumno.dni+'</li>')
             });
             
         },
